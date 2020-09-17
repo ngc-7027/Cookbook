ad6af4a1364a8d4ef53590ec6670e4300d2bc5bc
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 13:46:44 +0200

    2. Base Entity

---
+++ Cookbook.App/Cookbook.DAL/Entities/Base/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using Cookbook.DAL.Entities.Interfaces;
+using System;
+namespace Cookbook.DAL.Entities.Base
+{
+    public abstract class EntityBase : IEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
