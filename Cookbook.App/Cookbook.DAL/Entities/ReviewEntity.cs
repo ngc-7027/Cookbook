fc52efcf15921c66cb051c9c81a1efe5967238b4
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:23:58 +0200

    8. Review entity

---
+++ Cookbook.App/Cookbook.DAL/Entities/ReviewEntity.cs
@@ -0,0 +1,16 @@
+﻿using Cookbook.Common.Enums;
+using Cookbook.DAL.Entities.Base;
+using System;
+
+namespace Cookbook.DAL.Entities
+{
+    public class ReviewEntity : EntityBase
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ReviewText { get; set; }
+        public Rating Rating { get; set; }
+        public Guid RecipeId { get; set; }
+        public RecipeEntity Recipe { get; set; }
+    }
+}
