df6fff20f6c3fe95e80d6ff70573e39b5aaadab0
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:22:19 +0200

    5. IngredientAmount entity

---
+++ Cookbook.App/Cookbook.DAL/Entities/IngredientAmountEntity.cs
@@ -0,0 +1,16 @@
+﻿using Cookbook.Common.Enums;
+using Cookbook.DAL.Entities.Base;
+using System;
+
+namespace Cookbook.DAL.Entities
+{
+    public class IngredientAmountEntity : EntityBase
+    {
+        public double Amount { get; set; }
+        public Unit Unit { get; set; }
+        public Guid RecipeId { get; set; }
+        public RecipeEntity Recipe { get; set; }
+        public Guid IngredientId { get; set; }
+        public IngredientEntity Ingredient { get; set; }
+    }
+}
