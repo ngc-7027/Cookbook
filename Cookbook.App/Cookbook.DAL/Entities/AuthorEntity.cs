d891db42ed86f3ebfa8b3d9f1594e7799c24f49b
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:21:50 +0200

    4. Author entity

---
+++ Cookbook.App/Cookbook.DAL/Entities/AuthorEntity.cs
@@ -0,0 +1,13 @@
+﻿using Cookbook.DAL.Entities.Base;
+using System.Collections.Generic;
+
+namespace Cookbook.DAL.Entities
+{
+    public class AuthorEntity : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AboutAuthor { get; set; }
+        public ICollection<RecipeEntity> Recipes { get; set; }
+    }
+}
