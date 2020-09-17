976ab2a2992da6ed22c6b092667a0b69ff00774a
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:11:51 +0200

    FoodCategory enum

---
+++ Cookbook.App/Cookbook.Common/Enums/FoodCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Cookbook.Common.Enums
+{
+    public enum FoodCategory
+    {
+        Unknown = 0,
+        MainDish = 1,
+        Soup = 2,
+        Dessert = 3,
+        Salad = 4
+    }
+}
