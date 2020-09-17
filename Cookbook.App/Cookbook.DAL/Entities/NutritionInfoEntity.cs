63521fabbd60a43d6638586e1ca2709e1b631272
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:23:33 +0200

    7. NutritionInfo entity

---
+++ Cookbook.App/Cookbook.DAL/Entities/NutritionInfoEntity.cs
@@ -0,0 +1,13 @@
+﻿using Cookbook.DAL.Entities.Base;
+
+namespace Cookbook.DAL.Entities
+{
+    public class NutritionInfoEntity : EntityBase
+    {
+        public double PortionSize { get; set; }
+        public double Calories { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fat { get; set; }
+        public double Protein { get; set; }
+    }
+}
