4922294f74d0423b8c6ef013717934cf360b0cbe
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:14:31 +0200

    FoodSpecialDiet enum

---
+++ Cookbook.App/Cookbook.Common/Enums/FoodSpecialDiet.cs
@@ -0,0 +1,17 @@
+﻿namespace Cookbook.Common.Enums
+{
+    public enum FoodSpecialDiet
+    {
+        Unknown = 0,
+        Vegetarian = 1,
+        Vegan = 2,
+        LowCalorie = 3,
+        LowCholesterol = 4,
+        LowCarb = 5,
+        LowFat = 6,
+        GlutenFree = 7,
+        Diabetic = 8,
+        Keto = 9,
+        Protein = 10
+    }
+}
