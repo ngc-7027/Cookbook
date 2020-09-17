2459f9458ac18a231b33a87b0dc63b53e95c47f3
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-17 16:15:06 +0200

    Rating enum

---
+++ Cookbook.App/Cookbook.Common/Enums/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace Cookbook.Common.Enums
+{
+    public enum Rating
+    {
+        Unknown = 0,
+        OneStar = 1,
+        TwoStars = 2,
+        ThreeStars = 3,
+        FourStars = 4,
+        FiveStars = 5
+    }
+}
